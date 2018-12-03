590a2f27c7e61e6900e55f2e240b46de2c19bdd5
David <davo.melkumyan40@gmail.com>
2018-12-03 22:14:35 +0100

    Add project files.

---
+++ CodeSignalTask/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace CodeSignalTask
+{
+    public static class Extensions
+    {
+        public static bool IsPolindrom(this string someString)
+        {
+            if (someString == someString.ReverseString())
+                return true;
+            else
+                return false;
+        }
+
+        public static string ReverseString(this string str)
+        {
+            string s = null;
+            foreach (var item in str.Reverse())
+            {
+                s += item;
+            }
+            return s;
+        }
+    }
+}
---
+++ CodeSignalTask/Program.cs
@@ -0,0 +1,56 @@
+﻿// FINISHED WORK ANY POLINDROM
+
+using System;
+
+namespace CodeSignalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string not_Polindrom_String = "AAAASSDDDFFFF";
+            Console.WriteLine(AnyPolindrom(not_Polindrom_String));
+            Console.ReadKey();
+        }
+
+        public static bool CharChecker(string str)
+        {
+            char[] someArray = str.ToCharArray();
+            char previousLetter = ' ';
+            int letterCount = 0, oddCount = 0; ;
+            char letter = someArray[0];
+            for (int j = 0; j < someArray.Length; j++)
+            {
+                if(previousLetter != someArray[j])
+                {
+                    letter = someArray[j];
+                    for (int i = 0; i < someArray.Length; i++)
+                    {
+                        if (letter == someArray[i])
+                            letterCount++;
+                    }
+                    if (letterCount % 2 > 0)
+                        oddCount++;
+                    else
+                        previousLetter = letter;
+                    if (oddCount > 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool AnyPolindrom(string someString)
+        {
+            if (someString.IsPolindrom())
+            {
+                return true;
+            }
+            else
+                if (CharChecker(someString))
+                return true;
+
+            return false;
+        }
+    }
+}
